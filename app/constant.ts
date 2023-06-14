f6d0d6fce55647a2b6031d0242606aa7a318ad20
uni345 <32605011+uni345@users.noreply.github.com>
2023-06-14 08:12:36 +0000

    #

--- app/constant.ts
+++ app/constant.ts
@@ -1,10 +1,10 @@
-export const OWNER = "Licoy";
+export const OWNER = "#";
 export const REPO = "ChatGPT-Midjourney";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `${REPO_URL}#keep-updated`;
-export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
-export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
+export const REPO_URL = `#`;
+export const ISSUE_URL = `#"`;
+export const UPDATE_URL = `#"`;
+export const FETCH_COMMIT_URL = `#"`;
+export const FETCH_TAG_URL = `#"`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export enum Path {
